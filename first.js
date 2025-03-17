cf1f558f30d9fcc02521971384d1922f2e4a1401
Giuliana Bordon Castro <gbordon@ibm.com>
2025-03-17 20:28:54 -0300

    async

--- first.js
+++ first.js
@@ -1,5 +1,9 @@
 async function main(){
-    console.log("Hola from main");
+    const reg = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+    if(reg.ok){
+        const data = await reg.json();
+        console.log(data);
+    }
 
 }
 main();
